ab9a2026548cb1fe5af8dcdfe0b34692c9923a9f
Unknown <camilleotillio@gmail.com>
2018-11-01 10:01:07 -0700

    Added Images
    Targets changed

--- Projectile Dysfunction/Target.java
+++ Projectile Dysfunction/Target.java
@@ -7,11 +7,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version 1.0 30 October 2018
  */
 public class Target extends Actor {
+    private boolean turn;
     /**
      * Act - do whatever the Target wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() {
-        // Add your action code here.
-    }    
+
+        side();
+    }  
+    private void side()
+    {
+        if (getX() <=0)
+        {
+            turn = true;
+        }
+        if (getX()>=799){
+            turn = false;
+        } 
+        if (turn == false )
+        {
+            setLocation(getX()-5,getY());
+        }
+        if (turn == true)
+        {
+            setLocation(getX()+5,getY());
+        } 
+
+    }
 }
