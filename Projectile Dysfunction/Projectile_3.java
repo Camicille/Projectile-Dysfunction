32ae10b93a8d6f6aea66505f346abca52831f530
Unknown <camilleotillio@gmail.com>
2018-11-01 11:02:29 -0700

    Final Touches
    Please add return

--- Projectile Dysfunction/Projectile_3.java
+++ Projectile Dysfunction/Projectile_3.java
@@ -4,16 +4,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Write a description of class Projectile_3 here.
  * 
  * @author Camille Otillio
- * @version (a version number or a date)
+ * @version 1
  */
 public class Projectile_3 extends SmoothMover
 {
     /**
      * 
      */
-   private boolean readyToFire;
    
-    
    public Projectile_3()
     {
         this(new Vector(360, 12));
@@ -27,24 +25,25 @@ public class Projectile_3 extends SmoothMover
      */
     public void act() 
     {
+        int angle = Greenfoot.getRandomNumber(5)-10;
         addToVelocity(AWorld.GRAVITY);
         move();
+        turn(angle);
         collisionDetector();
+        
     }
    
     private void collisionDetector()
-    {
-            
+    {  
         Actor target = getOneIntersectingObject(Target.class);
         if (target != null)
-           {
-                for(int i = -2; i <= 3; i++){
-                    int angle = 40 * i;
-                    AWorld world = (AWorld)getWorld();
-                    world.addObject(new Projectile_3End(getRotation() + 90 * i, new Vector(90 + angle, 5)), getX(), getY());
-                }
-                getWorld().removeObject(this);
+         {
+            for(int i = -2; i <= 3; i++){
+                int angle = 40 * i;
+                AWorld world = (AWorld)getWorld();
+                world.addObject(new Projectile_3End(getRotation() + 90 * i, new Vector(90 + angle, 5)), getX(), getY());
             }
-              
+            getWorld().removeObject(this);
+        }
     }
 }
