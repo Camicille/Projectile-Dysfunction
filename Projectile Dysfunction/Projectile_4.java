1829cabf6cf98c7f39f155d0063404522e57bbd1
tylerbakeman <43856949+tylerbakeman@users.noreply.github.com>
2018-11-01 10:55:41 -0700

    Right Place Right time

--- Projectile Dysfunction/Projectile_4.java
+++ Projectile Dysfunction/Projectile_4.java
@@ -4,15 +4,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Write a description of class Projectile_4 here.
  * 
  * @author Tyler Bakeman
- * @version (a version number or a date)
+ * @version 10/31/2018
  */
 public class Projectile_4 extends SmoothMover
 {
-    /**
-     * 
-     */
-    public Projectile_4(Vector velocity) {
-        super(velocity);
+    private Vector velocity;
+    private int time = 0;
+    public Projectile_4(){
+        super(new Vector(45,2));
+        velocity = new Vector(45,2);
+        setImage("gun.png");
+        getImage().scale(75,75);
+        trimWhite();
+    }
+    public Projectile_4(Vector _velocity) {
+        super(_velocity);
+        velocity = _velocity;
+        setImage("gun.png");
+        getImage().scale(75,75);
+        trimWhite();
     }
     
     /**
@@ -21,7 +31,41 @@ public class Projectile_4 extends SmoothMover
      */
     public void act() 
     {
+        time++;
         addToVelocity(AWorld.GRAVITY);
+        addToVelocity(new Vector(-90,.05));
+        velocity.add(AWorld.GRAVITY);
         move();
+        main();
     }     
+    
+    public void main()
+    {
+        Target who = (Target)getOneIntersectingObject(Target.class);
+        if(time % 5 == 0){
+            Projectile_4End bullet = new Projectile_4End();
+            bullet.addToVelocity(velocity);
+            getWorld().addObject(bullet,(int)getExactX(),(int)getExactY());
+        }
+        if(who != null){
+            getWorld().removeObject(who);
+        }
+        if(getY() >= getWorld().getHeight() - 50){
+            getWorld().removeObject(this);
+        }
+        turn(7);
+    }
+    
+    public void trimWhite(){
+        int maxW = 235;
+        GreenfootImage img = getImage();
+        for(int y = 0; y < img.getHeight(); y++){
+            for(int x = 0; x < img.getWidth(); x++){
+                Color c = img.getColorAt(x,y);
+                if(c.getRed() > maxW && c.getGreen() > maxW && c.getBlue() > maxW)
+                    img.setColorAt(x,y,new Color(255,255,255,0)); //make transparent
+            }
+        }
+        setImage(img);
+    }
 }
--- Projectile Dysfunction/Projectile_4End.java
+++ Projectile Dysfunction/Projectile_4End.java
@@ -6,14 +6,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author Tyler Bakeman
  * @version (a version number or a date)
  */
-public class Projectile_4End extends Actor
-{
-    /**
-     * Act - do whatever the Projectile_4End wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+public class Projectile_4End extends SmoothMover{
+    public Projectile_4End(){
+        setImage("bullet.jpg");
+        getImage().scale(70,70);
+        setRotation(180);
+        trimWhite();
+    }
+    
     public void act() 
     {
-        // Add your action code here.
+        addToVelocity(new Vector(180,7));
+        move();
+        main();
     }    
+    
+    public void main()
+    {
+        Target who = (Target)getOneIntersectingObject(Target.class);
+        if(who != null){
+            getWorld().removeObject(who);
+            getWorld().removeObject(this);
+        } else if(isAtEdge()){
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void trimWhite(){
+        int maxW = 235;
+        GreenfootImage img = getImage();
+        for(int y = 0; y < img.getHeight(); y++){
+            for(int x = 0; x < img.getWidth(); x++){
+                Color c = img.getColorAt(x,y);
+                if(c.getRed() > maxW && c.getGreen() > maxW && c.getBlue() > maxW)
+                    img.setColorAt(x,y,new Color(255,255,255,0)); //make transparent
+            }
+        }
+        setImage(img);
+    }
 }
